30c12b20d8e3b0fa1e590753efa74aa1b2078524
hitenSharm <hiten.sharm1708@gmail.com>
2021-05-25 16:19:52 +0530

    make it better

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "help",
   execute(msg, args) {
     msg.channel.send(
-      " 1. !okbuddymeme command for viewing a meme \n2. !okbuddygif for a random gif \n3. !insult Generate an insult \n4. !kanyegod for a Kanye West quote\n5. Drop image with comment !anime to find out which anime it is\n6. !yoda + yourText to transelate to yoda speak"
+      " 1. !okbuddymeme command for viewing a meme \n2. !okbuddygif for a random gif \n3. !insult Generate an insult \n4. !kanyegod for a Kanye West quote\n5. Drop image with comment !anime to find out which anime it is\n6. !yoda + yourText to transelate to yoda speak\n7. Website:https://hiten-sharma-website.netlify.app/"
     );
   },
 };
