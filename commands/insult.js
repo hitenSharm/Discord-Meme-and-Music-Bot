a846ef2d3f3076bfee476ac304563e3bb0d88343
kyte <kyteinsky@gmail.com>
2021-10-01 12:57:04 +0530

    modified:	commands/insult.js
    Added a feature to send insult message for every argument supplied. Before this commit, only one insult message could be sent for any number of arguments.

--- commands/insult.js
+++ commands/insult.js
@@ -4,9 +4,17 @@ module.exports = {
   name: "insult",
   description: "Sends insults",
   async execute(msg, args) {
-    const reply = await grabInsult();
-    if(args.length===0)msg.reply(reply.toString());
-    else
-    msg.channel.send(reply.toString() + args[0]);
+    // reply to message if there is only one arg
+    if(args.length===0) {
+      const reply = await grabInsult();
+      msg.reply(reply.toString());
+      return;
+    }
+    
+    // else loop over args and send insults
+    for (let i = 0; i < args.length; i++) {
+      const reply = await grabInsult();
+      msg.channel.send(reply.toString() + args[i]);
+    }
   },
 };
