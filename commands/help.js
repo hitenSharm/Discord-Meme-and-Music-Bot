a29fb1d011022c033dd1cb9d4646dd19d882ad00
kailash360 <kailashkejriwal21@gmail.com>
2021-10-06 02:36:05 +0530

    Fix: Fixed embed object and spelling errors

--- commands/help.js
+++ commands/help.js
@@ -5,11 +5,8 @@ const embed = {
   author:{
     name: process.env.BOT ? `${process.env.BOT}`  :"OkBuddy"
   },
-  description : "You can use the following commands:\n1. !obmeme command for viewing a meme \n2. !obgif for a random gif \n3. !insult + tag the person Generate an insult \n4. !kanyegod for a Kanye West quote\n5. Drop image with comment !anime to find out which anime it is\n6. !yoda + yourText to transelate to yoda speak\n7. !obm play <song name> to play\n8. !obm skip to skip\n9. !obm stop to stop\n10. !obm pause to pause\n11. !obm resume to resume\n11. Website:https://hiten-sharma-website.netlify.app \n12.  Request a feature by creating na new issue at https://github.com/hitenSharm/Discord-Meme-and-Music-Bot/issues\n13. This is an open-source project. View the repository at https://github.com/hitenSharm/Discord-Meme-and-Music-Bot" ,
+  description : "You can use the following commands:\n1. !obmeme command for viewing a meme \n2. !obgif for a random gif \n3. !insult + tag the person Generate an insult \n4. !kanyegod for a Kanye West quote\n5. Drop image with comment !anime to find out which anime it is\n6. !yoda + yourText to transelate to yoda speak\n7. !obm play <song name> to play\n8. !obm skip to skip\n9. !obm stop to stop\n10. !obm pause to pause\n11. !obm resume to resume\n11. Website:https://hiten-sharma-website.netlify.app \n12.  Request a feature by creating a new issue at https://github.com/hitenSharm/Discord-Meme-and-Music-Bot/issues\n13. This is an open-source project. View the repository at https://github.com/hitenSharm/Discord-Meme-and-Music-Bot" ,
 	thumbnail: {
-		url: 'https://imgur.com/fs8xRSz.png',
-	},
-	image: {
 		url: 'https://imgur.com/fs8xRSz.png',
 	},
 	timestamp: new Date(),
